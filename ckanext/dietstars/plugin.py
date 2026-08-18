e715ef9c50e8c41fb3ff05d85c75285bedfd59b7
amercader <amercadero@gmail.com>
2026-08-18 14:35:36 +0200

    Remove unused resource template and helper

--- ckanext/dietstars/plugin.py
+++ ckanext/dietstars/plugin.py
@@ -25,16 +25,16 @@ def get_qa_dict(pkg_dict):
     qa_dict = {'openness_score_reason': '', 'openness_score': 0}
 
     license_id = pkg_dict.get('license_id')
-    
+
     if license_id is None:
         return {'openness_score': 0, 'openness_score_reason': "No license supplied"}
-    
+
     # pretty easy - first check if the license is open
     register = model.Package.get_license_register()
     if (not register.get(license_id) or register.get(license_id).isopen() == False) and license_id.lower() not in open_licenses:
         qa_dict['openness_score_reason'] = 'The dataset license is not in our list of Open Licenses.'
         return qa_dict
-    
+
 
     # now it's at least 1-star
     qa_dict['openness_score'] = 1
@@ -75,23 +75,6 @@ def get_qa_dict(pkg_dict):
     return qa_dict
 
 
-def qa_openness_stars_resource_html(resource):
-    qa = resource.get('qa')
-    if not qa:
-        return toolkit.literal('<!-- No qa info for this resource -->')
-    if not isinstance(qa, dict):
-        return toolkit.literal('<!-- QA info was of the wrong type -->')
-
-    # Take a copy of the qa dict, because weirdly the renderer appears to add
-    # keys to it like _ and app_globals. This is bad because when it comes to
-    # render the debug in the footer those extra keys take about 30s to render,
-    # for some reason.
-    extra_vars = copy.deepcopy(qa)
-    return toolkit.literal(
-        toolkit.render('qa/openness_stars.html',
-                  extra_vars=extra_vars))
-
-
 def qa_openness_stars_dataset_html(dataset):
     qa = dataset.get('qa')
     if not qa:
@@ -123,7 +106,6 @@ class DietStarsPlugin(plugins.SingletonPlugin):
 
     def get_helpers(self):
         return {
-            'qa_openness_stars_resource_html': qa_openness_stars_resource_html,
             'qa_openness_stars_dataset_html': qa_openness_stars_dataset_html,
             'get_qa_dict': get_qa_dict
         }
